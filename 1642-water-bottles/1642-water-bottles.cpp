424ee7f83b08d1f63f8b2dfc8711e0589ab9034f
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2024-07-08 00:50:06 +0530

    Time: 3 ms (13.78%), Space: 7 MB (72.3%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        return numBottles + (numBottles - 1)/(numExchange - 1);
+        // return numBottles + (numBottles - 1)/(numExchange - 1);
+        int ans = numBottles;
+        int emp = numBottles;
+
+        while(emp/numExchange != 0){
+            int val = emp/numExchange;
+            int nemp = emp - val*numExchange;
+            ans += val;
+            emp = val + nemp;
+        }
+        return ans;
     }
 };
