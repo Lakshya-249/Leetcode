167310cafe0d2b40d1a1baa83ad443cefa98f800
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2024-07-15 13:54:19 +0530

    Time: 701 ms (56.99%), Space: 282.7 MB (49.56%) - LeetHub

--- 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
@@ -13,19 +13,18 @@ class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
         unordered_map<int,TreeNode*> mpp;
-        unordered_set<int> parent;
         int n = descriptions.size();
         for(auto it: descriptions){
-            if(mpp.find(it[0])==mpp.end()) mpp[it[0]] = new TreeNode(it[0]);
             if(mpp.find(it[1])==mpp.end()) mpp[it[1]] = new TreeNode(it[1]);
-            parent.insert(it[1]);
         }
-
         TreeNode* root;
         for(auto it: descriptions){
+            if(mpp.find(it[0])==mpp.end()) {
+                mpp[it[0]] = new TreeNode(it[0]);
+                root = mpp[it[0]];
+            }
             if(it[2]) mpp[it[0]]->left = mpp[it[1]];
             else mpp[it[0]]->right = mpp[it[1]];
-            if(parent.find(it[0]) == parent.end()) root = mpp[it[0]];
         }
         return root;
     }
