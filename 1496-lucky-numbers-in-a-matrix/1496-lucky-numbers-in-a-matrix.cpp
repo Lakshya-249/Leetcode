1741d53ba723c2f743a58071581416318acea50a
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2024-07-19 20:03:30 +0530

    Time: 22 ms (7.85%), Space: 15 MB (5.19%) - LeetHub

---
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        vector<int>small,big;
+        for(auto it: matrix){
+            int smallnum = INT_MAX;
+            // cout<<smallnum;
+            for(auto itr:it){
+                smallnum = min(smallnum,itr);
+            }
+            small.push_back(smallnum);
+        }
+        cout<<small[0];
+        int n = matrix[0].size();
+        int m = matrix.size();
+        for(int i=0;i<n;i++){
+            int bignum = 0;
+            for(int j=0;j<m;j++){
+                bignum = max(bignum,matrix[j][i]);
+            }
+            big.push_back(bignum);
+        }
+        vector<int>ans;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == small[i] && matrix[i][j] == big[j]){
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
