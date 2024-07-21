3eb62debb4d18760a521fc62d37e1dcebd6a6bf3
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2024-07-22 03:53:46 +0530

    Time: 83 ms (94.44%), Space: 61.1 MB (73.89%) - LeetHub

---
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    vector<int> toposort(vector<vector<int>>& conditions, int k) {
+        vector<vector<int>> graph(k + 1);
+        vector<int> indegree(k + 1, 0);
+        for (auto& condition : conditions) {
+            graph[condition[0]].push_back(condition[1]);
+            ++indegree[condition[1]];
+        }
+        
+        queue<int> q;
+        for (int i = 1; i <= k; ++i) {
+            if (indegree[i] == 0) q.push(i);
+        }
+        
+        vector<int> order;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            order.push_back(node);
+            for (int neighbor : graph[node]) {
+                if (--indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+        return order.size() == k ? order : vector<int>();
+    }
+public:
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+        vector<int> rowOrder = toposort(rowConditions,k);
+        vector<int> colOrder = toposort(colConditions,k);
+
+        for(auto it:rowOrder) cout<<it<<" ";
+        cout<<endl;
+        for(auto it:colOrder) cout<<it<<" ";
+
+        if(rowOrder.empty() || colOrder.empty()) return {};
+        vector<vector<int>>ans(k,vector<int>(k,0));
+        unordered_map<int,int>rowMap,colMap;
+        for(int i=0;i<k;i++){
+            rowMap[rowOrder[i]] = i;
+            colMap[colOrder[i]] = i;
+        }
+        for(int i=1;i<=k;i++){
+            ans[rowMap[i]][colMap[i]] = i;
+        }
+        return ans;
+    }
+};
