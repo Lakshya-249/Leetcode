39e415aa9635750b73705d3a7d1b3c4254b66b18
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2024-07-26 19:36:37 +0530

    Time: 24 ms (74.13%), Space: 16.1 MB (28.76%) - LeetHub

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>>graph(n,vector<int>(n,1e9));
+        for(auto it: edges){
+            graph[it[0]][it[1]] = it[2];
+            graph[it[1]][it[0]] = it[2];
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j) graph[i][j] = 0;
+                for(int k=0;k<n;k++){
+                    // if((graph[i][k]) == 1e9 && (graph[k][j] == 1e9)) continue;
+                    graph[j][k] = min(graph[j][k],graph[j][i]+graph[i][k]);
+                }
+            }
+        }
+        // for(auto it: graph){
+        //     for(auto itr: it){
+        //         cout<<itr<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // unordered_map<int,int>mpp;
+        int ans = 0;
+        int min_num = n;
+        for(int i=0;i<n;i++){
+            int val = 0;
+            for(int j=0;j<n;j++){
+                if(graph[i][j] <= distanceThreshold) ++val;
+            }
+            // cout<<val<<endl;
+            if(val<=min_num){
+                min_num = val;
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
