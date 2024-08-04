cb3ee87f156aebfd73d88001512cbc66e0584bf4
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2024-08-04 19:57:17 +0530

    Time: 0 ms (100%), Space: 8.3 MB (32.57%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,11 +3,10 @@ public:
     int reverse(int x) {
         long long ans = 0;
         while(x!=0){
-            if((ans>INT_MAX) || (ans<INT_MIN)) return 0;
-            ans += (x%10);
-            ans *= 10;
+            if((ans>INT_MAX/10) || (ans<INT_MIN/10)) return 0;
+            ans = (ans*10)+(x%10);
             x = x/10;
         }
-        return ans/10;
+        return ans;
     }
 };
