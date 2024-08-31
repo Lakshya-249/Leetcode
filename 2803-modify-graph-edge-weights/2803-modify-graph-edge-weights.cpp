9797678c1c778b8a3baa701e744c443a5b8d8cbd
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2024-08-31 20:27:01 +0530

    Time: 936 ms (18.62%), Space: 575.6 MB (8.5%) - LeetHub

--- 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
+++ 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
@@ -1,69 +1,63 @@
-class Solution {    
+class Solution {
 public:
     vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
-        int INF = 2e9;
-        unordered_map<int, vector<pair<int, int>>> nodeNeighMp;
+        unordered_map<int, vector<pair<int, int>>> adj;
         
         for (auto& edge : edges) {
             int firstNode = edge[0], secondNode = edge[1], edgWt = edge[2];
             if (edgWt == -1) continue;
-            nodeNeighMp[firstNode].emplace_back(secondNode, edgWt);
-            nodeNeighMp[secondNode].emplace_back(firstNode, edgWt);
+            adj[firstNode].emplace_back(secondNode, edgWt);
+            adj[secondNode].emplace_back(firstNode, edgWt);
         }
-        
-        int srcDestDist = findShortestDist(n, source, destination, nodeNeighMp);
-        
-        if (srcDestDist < target) {
-            return {};
-        }
-        
-        bool distEqToTrgt = srcDestDist == target;
-        
-        for (int indx = 0; indx < edges.size(); indx++) {
-            auto& edge = edges[indx];
-            int firstNode = edge[0], secondNode = edge[1], edgWt = edge[2];
-            if (edgWt != -1) continue;
-            
-            edgWt = distEqToTrgt ? INF : 1;
-            nodeNeighMp[firstNode].emplace_back(secondNode, edgWt);
-            nodeNeighMp[secondNode].emplace_back(firstNode, edgWt);
-            
-            if (!distEqToTrgt) {
-                srcDestDist = findShortestDist(n, source, destination, nodeNeighMp);
-                if (srcDestDist <= target) {
-                    edgWt += (target - srcDestDist);
-                    distEqToTrgt = true;
-                }
-            }
-            
-            edge[2] = edgWt;
-        }
-        
-        return distEqToTrgt ? edges : vector<vector<int>>{};
-    }
+        int dist = shortestPath(adj,n,source,destination);
 
-private:
-    int findShortestDist(int n, int startNode, int endNode, unordered_map<int, vector<pair<int, int>>>& nodeNeighMp) {
-        int INF = 2e9;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minDisFrmSrcHeap;
-        vector<int> distFrmSrc(n, INF);
-        distFrmSrc[startNode] = 0;
-        minDisFrmSrcHeap.emplace(distFrmSrc[startNode], startNode);
+        if(dist < target) return {};
         
-        while (!minDisFrmSrcHeap.empty()) {
-            auto nodePr = minDisFrmSrcHeap.top();
-            minDisFrmSrcHeap.pop();
-            int nodeVal = nodePr.second, nodeDisFrmSrc = nodePr.first;
-            if (!nodeNeighMp.count(nodeVal)) continue;
-            for (auto& neighPr : nodeNeighMp[nodeVal]) {
-                int neighVal = neighPr.first, neighNodeDist = neighPr.second;
-                if (neighNodeDist + nodeDisFrmSrc < distFrmSrc[neighVal]) {
-                    distFrmSrc[neighVal] = neighNodeDist + nodeDisFrmSrc;
-                    minDisFrmSrcHeap.emplace(distFrmSrc[neighVal], neighVal);
+        bool isTarget = (dist == target);
+
+        for(int ind=0;ind<edges.size();ind++){
+            int i = edges[ind][0],j = edges[ind][1],wt = edges[ind][2];
+            if(wt != -1) continue;
+
+            wt = isTarget ? 1e9 : 1;
+            // cout<<wt<<" ";
+            adj[i].push_back({j,wt});
+            adj[j].push_back({i,wt});
+            
+            if(!isTarget){
+                dist = shortestPath(adj,n,source,destination);
+                if(dist <= target){
+                    wt += (target - dist);
+                    isTarget = true;
                 }
             }
+            edges[ind][2] = wt;
+            cout<<wt<<endl;
         }
-        
-        return distFrmSrc[endNode];
+        // for(auto it: edges){
+        //     cout<<it[0]<<" "<<it[1]<<" "<<it[2]<<endl;
+        // }
+        return isTarget ? edges : vector<vector<int>>{};
+    }
+private:
+    int shortestPath(unordered_map<int, vector<pair<int, int>>> adj,int n,int source,int destination){
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        vector<int>dist(n,INT_MAX);
+        dist[source] = 0;
+        pq.push({dist[source],source});
+
+        while(!pq.empty()){
+            auto [wt,node] = pq.top();
+            pq.pop();
+            for(auto [i,j]: adj[node]){
+                if(dist[i]>wt+j){
+                    dist[i] = wt+j;
+                    pq.push({dist[i],i});
+                }
+                // cout<<dist[i]<<" ";
+            }
+        } 
+        cout<<dist[destination]<<"yes ";
+        return dist[destination];
     }
 };
