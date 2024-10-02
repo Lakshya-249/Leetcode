2b6798e8887bc8958bdd609a347374ead688d8c7
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2024-10-02 17:03:42 +0530

    Time: 105 ms (24.32%), Space: 51.4 MB (8.3%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,23 +1,17 @@
 class Solution {
-    vector<int>vec;
-    int findRank(int num){
-        int l = 0,r = vec.size()-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(vec[mid] == num) return mid+1;
-            else if(vec[mid] > num) r = mid-1;
-            else l = mid+1;
-        }
-        return -1;
-    }
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {  
         set<int>st;
+        unordered_map<int,int>mpp;
         for(auto it: arr) st.insert(it);
-        for(auto it: st) vec.push_back(it);
+        int n = 1;
+        for(auto it: st){
+            mpp[it] = n;
+            n++;
+        }
         vector<int>res;
         for(auto it:arr){
-            int ind = findRank(it);
+            int ind = mpp[it];
             res.push_back(ind);
         }
         return res;
