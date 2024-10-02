84051c6a41e15c7fe084f8b269c773de2a6712b9
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2024-10-02 16:59:18 +0530

    Time: 89 ms (41.66%), Space: 44.1 MB (41.66%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -4,7 +4,6 @@ class Solution {
         int l = 0,r = vec.size()-1;
         while(l<=r){
             int mid = (l+r)/2;
-            // cout<<mid<<" ";
             if(vec[mid] == num) return mid+1;
             else if(vec[mid] > num) r = mid-1;
             else l = mid+1;
