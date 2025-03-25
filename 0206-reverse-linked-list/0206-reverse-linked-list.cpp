7f95f777bbbc4fc91b2cc726d3f4be758fc2e0e7
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2025-03-25 13:59:38 +0530

    Time: 0 ms (100%), Space: 13.5 MB (18.11%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -9,17 +9,26 @@
  * };
  */
 class Solution {
+    ListNode* reverseL(ListNode* prev, ListNode* node){
+        if(node == nullptr){
+            return prev;
+        }
+        ListNode* temp = node->next;
+        node->next = prev;
+        return reverseL(node, temp);
+    }
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while(curr != nullptr){
-            ListNode* temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            head = curr;
-            curr = temp;
-        }
-        return head;
+        // ListNode* prev = nullptr;
+        // ListNode* curr = head;
+        // while(curr != nullptr){
+        //     ListNode* temp = curr->next;
+        //     curr->next = prev;
+        //     prev = curr;
+        //     head = curr;
+        //     curr = temp;
+        // }
+        ListNode* node = reverseL(nullptr, head);
+        return node;
     }
 };
