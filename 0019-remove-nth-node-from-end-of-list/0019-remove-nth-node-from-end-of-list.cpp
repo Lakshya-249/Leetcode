0eb77a7602f7d99fd59a0ab623b4059088e67909
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2025-03-27 17:35:32 +0530

    Time: 0 ms (100%), Space: 15 MB (59.77%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,27 +11,36 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        stack<ListNode*>st;
-        ListNode* node = head;
-        while(node != nullptr){
-            st.push(node);
-            node = node->next;
+        // stack<ListNode*>st;
+        // ListNode* node = head;
+        // while(node != nullptr){
+        //     st.push(node);
+        //     node = node->next;
+        // }
+        // ListNode* prev = nullptr;
+        // while(!st.empty()){
+        //     n--;
+        //     if(n==0){
+        //         st.pop();
+        //         if(st.empty()) head = prev;
+        //         else {
+        //             ListNode* temp = st.top();
+        //             temp->next = prev;
+        //         }
+        //         break;
+        //     }
+        //     prev = st.top();
+        //     st.pop();
+        // }
+        // return head;
+        ListNode* dnode = new ListNode(0, head);
+        ListNode* ans = dnode;
+        for (int i=0; i<n; i++) head = head->next;
+        while(head != nullptr){
+            dnode = dnode->next;
+            head = head->next;
         }
-        ListNode* prev = nullptr;
-        while(!st.empty()){
-            n--;
-            if(n==0){
-                st.pop();
-                if(st.empty()) head = prev;
-                else {
-                    ListNode* temp = st.top();
-                    temp->next = prev;
-                }
-                break;
-            }
-            prev = st.top();
-            st.pop();
-        }
-        return head;
+        dnode->next = dnode->next->next;
+        return ans->next;
     }
 };
