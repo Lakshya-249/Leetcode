8c2d9bee285df1d47c55d57b30bf252f88f19bff
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2025-03-28 14:14:21 +0530

    Time: 54 ms (21%), Space: 75.7 MB (35.3%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -9,24 +9,59 @@
  * };
  */
 class Solution {
+    ListNode* merge(ListNode* first, ListNode* second){
+        ListNode* ans = new ListNode(0);
+        ListNode* dummy = ans;
+        while(first != nullptr && second != nullptr){
+            if (first->val < second->val){
+                dummy->next = first;
+                first = first->next;
+            }else {
+                dummy->next = second;
+                second = second->next;
+            }
+            dummy = dummy->next;
+        }
+        if (first != nullptr){
+            dummy->next = first;
+        }
+        if(second != nullptr) {
+            dummy->next = second;
+        }
+        return ans->next;
+    }
 public:
     ListNode* sortList(ListNode* head) {
-        vector<ListNode*> lst;
-        ListNode* node = head;
-        while(node != nullptr) {
-            lst.push_back(node);
-            node = node->next;
+        // vector<ListNode*> lst;
+        // ListNode* node = head;
+        // while(node != nullptr) {
+        //     lst.push_back(node);
+        //     node = node->next;
+        // }
+        // sort(lst.begin(),lst.end(),[&](ListNode* node1,ListNode* node2){
+        //     return node1->val < node2->val;
+        // });
+        // ListNode* ans = new ListNode();
+        // node = ans;
+        // for (int i=0;i<lst.size();i++){
+        //     node->next = lst[i];
+        //     node = node->next;
+        // }
+        // node->next = nullptr;
+        // return ans->next;
+        if (head == nullptr || head->next == nullptr) return head;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        sort(lst.begin(),lst.end(),[&](ListNode* node1,ListNode* node2){
-            return node1->val < node2->val;
-        });
-        ListNode* ans = new ListNode();
-        node = ans;
-        for (int i=0;i<lst.size();i++){
-            node->next = lst[i];
-            node = node->next;
-        }
-        node->next = nullptr;
-        return ans->next;
+
+        ListNode* secondNode = slow->next;
+        slow->next = nullptr;
+        ListNode* first = sortList(head);
+        ListNode* second = sortList(secondNode);
+        return merge(first,second);
     }
 };
