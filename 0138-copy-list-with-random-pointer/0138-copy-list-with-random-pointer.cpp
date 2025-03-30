1ae53e97c52fca028f8a8d7bc5eff8afd8662504
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2025-03-30 17:59:33 +0530

    Time: 7 ms (53.35%), Space: 15.1 MB (50.86%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,18 +17,45 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*>mpp;
+        // unordered_map<Node*, Node*>mpp;
+        // Node* node = head;
+        // while(node){
+        //     mpp[node] = new Node(node->val);
+        //     node = node->next;
+        // }
+        // node = head;
+        // while(node){
+        //     mpp[node]->next = mpp[node->next];
+        //     mpp[node]->random = mpp[node->random];
+        //     node = node->next;
+        // }
+        // return mpp[head];
+        if(!head) return head; 
         Node* node = head;
         while(node){
-            mpp[node] = new Node(node->val);
-            node = node->next;
+            Node* temp = new Node(node->val);
+            temp->next = node->next;
+            node->next = temp;
+            node = temp->next;
         }
         node = head;
         while(node){
-            mpp[node]->next = mpp[node->next];
-            mpp[node]->random = mpp[node->random];
-            node = node->next;
+            Node* rand = node->random;
+            node->next->random = rand ? rand->next : rand;
+            node = node->next->next;
         }
-        return mpp[head];
+        
+        Node* newHead = head->next;
+        Node* copy = newHead;
+        node = head;
+
+        while (node) {
+            node->next = node->next->next;
+            copy->next = copy->next ? copy->next->next : nullptr;
+            node = node->next;
+            copy = copy->next;
+        }
+
+        return newHead; 
     }
 };
