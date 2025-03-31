e72b3665da5c5b98eeb51ce0a1a94a8e82d81d93
Lakshya Kumar Singh <64345392+Lakshya-249@users.noreply.github.com>
2025-03-31 23:26:38 +0530

    Time: 0 ms (100%), Space: 30.4 MB (25.27%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,25 +1,16 @@
 class Solution {
-    void rotL(vector<int>& nums, int k , int n){
-        if (!k) return;
-        int temp = nums[n-1];
-        for (int i=n-1;i>0;i--) nums[i] = nums[i-1];
-        nums[0] = temp;
-        rotL(nums, k-1, n);
-    }
 public:
     void rotate(vector<int>& nums, int k) {
         // int n = nums.size();
-        // if (n==1) return;
-        // cout<<n;
-        // rotL(nums, k%n , n);
+        // k = k % n; 
+        // reverse(nums.begin(), nums.end());
+        // reverse(nums.begin(), nums.begin() + k);
+        // reverse(nums.begin() + k, nums.end());
         int n = nums.size();
-        k = k % n; // Handle cases where k > n
-
-        // Reverse the entire array
-        reverse(nums.begin(), nums.end());
-        // Reverse first k elements
-        reverse(nums.begin(), nums.begin() + k);
-        // Reverse the remaining n-k elements
-        reverse(nums.begin() + k, nums.end());
+        vector<int>temp(n,0);
+        for(int i=0;i<n;i++){
+            temp[(i+k) % n] = nums[i];
+        }
+        nums = temp;
     }
 };
